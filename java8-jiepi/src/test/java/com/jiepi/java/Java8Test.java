a12f420029304c8d6a91ae9e4afa3c79d87bbbb0
wangxuan <wangxuan@finogeeks.com>
2019-06-20 23:15:33 +0800

    stream

--- java8-jiepi/src/test/java/com/jiepi/java/Java8Test.java
+++ java8-jiepi/src/test/java/com/jiepi/java/Java8Test.java
@@ -3,9 +3,8 @@ package com.jiepi.java;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Java8Test {
@@ -62,5 +61,80 @@ public class Java8Test {
                 .distinct()//实现equal hashcode
                 .forEach(System.out::println);
     }
+
+
+    @Test
+    public void test4(){
+        List<String>  list = Arrays.asList("aaa","bbb","ccc");
+        list.stream().map((s)->s.toUpperCase())
+                .forEach(System.out::println);
+
+        /**
+         * flatmap 输入一个函数参数， 多个stream和成一个stream
+         */
+   list.stream()
+           .flatMap(Java8Test::filter).forEach(System.out::println);
+    list.stream().flatMap((str)->filter(str)).forEach(System.out::println);
+
+    }
+
+    public static Stream<Character> filter(String str){
+        List<Character> list = new ArrayList<>();
+
+        for (Character ch:str.toCharArray()
+             ) {
+                list.add(ch);
+        }
+        return list.stream();
+    }
+
+    @Test
+    public void  test7(){
+
+        boolean b = list.stream().allMatch(it -> it.getAge() >=10);
+        System.out.println(b);
+
+        boolean wang = list.stream().anyMatch(it -> it.getName().contains("li"));
+        System.out.println(wang);
+
+        Optional<Employee> any = list.parallelStream().findAny();
+        System.out.println(any.get());
+        Optional<Employee> first = list.stream().filter(it -> it.getAge() > 10).findFirst();
+
+        System.out.println(first.get());
+
+    }
+
+
+    List<Employee> listemp = Arrays.asList(
+            new Employee(10, "lisi", "男"),
+            new Employee(12, "流入", "男"),
+            new Employee(13, "王二", "女"),
+            new Employee(16, "lisi", "男"),
+            new Employee(13, "lisi", "男"),
+            new Employee(14, "lisi", "男"),
+            new Employee(14, "lisi", "男"),
+            new Employee(14, "lisi", "男"),
+            new Employee(18, "lisi", "男")
+    );
+
+    @Test
+    public void test8(){
+        listemp.stream().mapToInt(e->e.getAge()).forEach(System.out::println);
+
+        Map<Integer, List<Employee>> collect = listemp.stream().collect(Collectors.groupingBy((e) -> e.getAge()));
+
+        System.out.println(collect);
+        Map<Boolean, List<Employee>> collect1 = listemp.stream().collect(Collectors.partitioningBy(e -> e.getAge() > 12));
+
+        System.out.println(collect1);
+
+        Map<Integer,Map<String,List<Employee>>> t=listemp.stream().collect(Collectors.groupingBy(e->e.getAge(),Collectors.groupingBy(s->s.getSex())));
+
+        System.out.println(t);
+
+
+    }
+
 
 }
